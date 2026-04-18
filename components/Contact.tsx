4fba3331c6a513ce824c289753321edf369df44b
valeriaanaf <155537342+ValerianAhmad@users.noreply.github.com>
2026-04-18 10:43:01 +0700

    .

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -51,7 +51,7 @@ ${formData.message ? `Keluhan/Keterangan: ${formData.message}` : ''}`;
       icon: <MapPin size={24} />,
       title: "Alamat",
       content: "Jl. Raya Jatiasih, Kec. Jatiasih, Kota Bekasi, Jawa Barat",
-      link: "https://maps.app.goo.gl/CJ2Pfhggz4GzBqt46"
+      link: "https://maps.app.goo.gl/giUpMqKeJq9uRriaA"
     },
     {
       icon: <Phone size={24} />,
@@ -122,7 +122,7 @@ ${formData.message ? `Keluhan/Keterangan: ${formData.message}` : ''}`;
             {/* Map */}
             <div className="rounded-xl overflow-hidden shadow-lg">
               <iframe
-                src="https://maps.app.goo.gl/CJ2Pfhggz4GzBqt46"
+                src="https://maps.google.com/maps?q=Sugi+Ac+Mobil,+Bekasi&t=&z=15&ie=UTF8&iwloc=&output=embed"
                 width="100%"
                 height="300"
                 style={{ border: 0 }}
